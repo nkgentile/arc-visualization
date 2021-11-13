685fb3902ff235b479ddbdd78768640e3c512023
Noah Gentile <noah.gentile@gmail.com>
2021-11-13 12:31:31 -0800

    using promisified stream pipeline

--- arc-to-geojson.js
+++ arc-to-geojson.js
@@ -4,26 +4,26 @@ import EventStream from 'event-stream';
 const { map } = EventStream;
 import reduce from 'stream-reduce';
 import { parse, stringify } from 'JSONStream';
+import { pipeline } from 'stream/promises';
 
 const args = process.argv;
 const jsonFilename = args[2];
-assert(jsonFilename?.length > 0, 'Please provide a path to a JSON file');
+assert(jsonFilename?.length > 0 && jsonFilename.endsWith('json'), 'Please provide a path to a JSON file');
 
-const logStream = createWriteStream("log.txt", { flags: 'a' });
-
-const geoJSON = createReadStream(jsonFilename, { encoding: 'utf8' })
-    .pipe(parse('timelineItems.*'))
-    .pipe((map(function (item, callback) {
+await pipeline(
+    createReadStream(jsonFilename, { encoding: 'utf8' }),
+    parse('timelineItems.*'),
+    map(function (item, callback) {
         try {
             if (!Array.isArray(item?.samples) || item?.samples?.length === 0) {
-                logStream.write(`Skipping item ${item.itemId}\n`);
+                process.stderr.write(`Skipping item ${item.itemId}\n`);
                 callback();
                 return;
             }
 
             const coordinates = item.samples.reduce((coordinates, sample) => {
                 if (!(Number.isFinite(sample?.location?.longitude) && Number.isFinite(sample?.location?.latitude))) {
-                    logStream.write(`Skipping sample ${sample?.sampleId}\n`);
+                    process.stderr.write(`Skipping sample ${sample?.sampleId}\n`);
                     return coordinates;
                 }
 
@@ -46,16 +46,15 @@ const geoJSON = createReadStream(jsonFilename, { encoding: 'utf8' })
         } catch (error) {
             callback(error);
         }
-    })))
-    .pipe(reduce((featureCollection, feature) => {
+    }),
+    reduce((featureCollection, feature) => {
         featureCollection.features.push(feature);
         return featureCollection;
-    }, {
-        type: "FeatureCollection",
-        features: []
-    }))
-    .pipe(stringify('', ',\n', ''))
-    .pipe(process.stdout);
+    }, { type: "FeatureCollection", features: [] }),
+    stringify('', ',\n', ''),
+    process.stdout
+)
+
 
 // If we use `head` downstream it will send an error signal
 process.stdout.on('error', process.exit);
