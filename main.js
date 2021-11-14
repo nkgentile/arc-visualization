00229259cc278c6af7451cf78038cdaef92f7bb8
Noah Gentile <noah.gentile@gmail.com>
2021-11-14 12:29:15 -0800

    adding tailwind

--- main.js
+++ main.js
@@ -2,69 +2,73 @@ import './style.css';
 import { create, zoom as d3Zoom, zoomIdentity } from 'd3';
 import { geoMercator, geoPath } from 'd3-geo';
 import { json } from 'd3-fetch';
-import { tile as d3Tile } from 'd3-tile';
+// import { tile as d3Tile } from 'd3-tile';
 const jsonUrl = new URL('./test.json', import.meta.url).href;
 
-const url = (x, y, z) => `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/${z}/${x}/${y}${devicePixelRatio > 1 ? "@2x" : ""}?access_token=pk.eyJ1IjoibWJvc3RvY2siLCJhIjoiY2s5ZWRlbTM4MDE0eDNocWJ2aXR2amNmeiJ9.LEyjnNDr_BrxRmI4UDyJAQ`;
+// const url = (x, y, z) => `https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/${z}/${x}/${y}${devicePixelRatio > 1 ? "@2x" : ""}?access_token=pk.eyJ1IjoibWJvc3RvY2siLCJhIjoiY2s5ZWRlbTM4MDE0eDNocWJ2aXR2amNmeiJ9.LEyjnNDr_BrxRmI4UDyJAQ`;
 const width = 600;
 const height = 600;
-const initialCenter = [-98 - 35 / 60, 39 + 50 / 60];
-const initialScale = 1 << 12;
-// const topology = await json("https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json");
-const featureCollection = await json(jsonUrl);
+const initialCenter = [-122.4687339913859, 37.764935808126985];
+const initialScale = 1 << 22;
 
-const svg = create("svg")
-    .attr("viewBox", [0, 0, width, height]);
+async function render() {
+    const featureCollection = await json(jsonUrl);
 
-const projection = geoMercator()
-    .scale(1 / (2 * Math.PI))
-    .translate([0, 0]);
+    const svg = create("svg")
+        .attr("viewBox", [0, 0, width, height]);
 
-const render = geoPath(projection);
+    const projection = geoMercator()
+        .scale(1 / (2 * Math.PI))
+        .translate([0, 0]);
 
-const tile = d3Tile()
-    .extent([[0, 0], [width, height]])
-    .tileSize(512);
+    const render = geoPath(projection);
 
-const zoom = d3Zoom()
-    .scaleExtent([1 << 10, 1 << 15])
-    .extent([[0, 0], [width, height]])
-    .on("zoom", ({ transform }) => zoomed(transform));
+    // const tile = d3Tile()
+    //     .extent([[0, 0], [width, height]])
+    //     .tileSize(512);
 
-let image = svg.append("g")
-    .attr("pointer-events", "none")
-    .selectAll("image");
+    const zoom = d3Zoom()
+        .scaleExtent([1 << 16, 1 << 32])
+        .extent([[0, 0], [width, height]])
+        .on("zoom", ({ transform }) => zoomed(transform));
 
-const path = svg.append("path")
-    .attr("pointer-events", "none")
-    .attr("fill", "none")
-    .attr("stroke", "red")
-    .attr("stroke-linecap", "round")
-    .attr("stroke-linejoin", "round");
+    // let image = svg.append("g")
+    //     .attr("pointer-events", "none")
+    //     .selectAll("image");
 
-svg
-    .call(zoom)
-    .call(zoom.transform, zoomIdentity
-        .translate(width / 2, height / 2)
-        .scale(-initialScale)
-        .translate(...projection(initialCenter))
-        .scale(-1));
+    const path = svg.append("path")
+        .attr("pointer-events", "none")
+        .attr("fill", "none")
+        .attr("stroke", "red")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-linejoin", "round");
 
-function zoomed(transform) {
-    const tiles = tile(transform);
+    svg
+        .call(zoom)
+        .call(zoom.transform, zoomIdentity
+            .translate(width / 2, height / 2)
+            .scale(-initialScale)
+            .translate(...projection(initialCenter))
+            .scale(-1));
 
-    image = image.data(tiles, d => d).join("image")
-        // .attr("xlink:href", d => url(...d))
-        .attr("x", ([x]) => (x + tiles.translate[0]) * tiles.scale)
-        .attr("y", ([, y]) => (y + tiles.translate[1]) * tiles.scale)
-        .attr("width", tiles.scale)
-        .attr("height", tiles.scale);
+    function zoomed(transform) {
+        // const tiles = tile(transform);
 
-    projection
-        .scale(transform.k / (2 * Math.PI))
-        .translate([transform.x, transform.y]);
+        // image = image.data(tiles, d => d).join("image")
+        //     // .attr("xlink:href", d => url(...d))
+        //     .attr("x", ([x]) => (x + tiles.translate[0]) * tiles.scale)
+        //     .attr("y", ([, y]) => (y + tiles.translate[1]) * tiles.scale)
+        //     .attr("width", tiles.scale)
+        //     .attr("height", tiles.scale);
 
-    path.attr("d", render(featureCollection));
+        projection
+            .scale(transform.k / (2 * Math.PI))
+            .translate([transform.x, transform.y]);
+
+        path.attr("d", render(featureCollection));
+    }
+
+    document.body.appendChild(svg.node());
 }
 
-document.querySelector('#app').appendChild(svg.node());
+render().catch(console.error);
---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: 'jit',
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    // classes that are generated dynamically, e.g. `rounded-${size}` and must
+    // be kept
+    safeList: [],
+    content: [
+      './index.html',
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
